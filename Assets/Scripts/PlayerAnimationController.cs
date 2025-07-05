e91c7c81efcddb62c015ddbf422597bf99849a47
sh010207 <sh010207@naver.com>
2025-07-05 21:24:12 +0900

    Feat : Player 이동 구현 , 방향 설정, animation 설정
    Pakage : Cinemachine Install
    Chore: Cinemachine를 통해 player 따라다니게 설정

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private readonly int IsRun = Animator.StringToHash("IsRun");
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    public void MoveAnimation(bool isMove)
+    {
+        if (isMove)
+        {
+            _animator.SetInteger(IsRun, 1);
+        }
+        else
+        {
+            _animator.SetInteger(IsRun, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private PlayerAnimationController animationController;
+
+    private Vector2 lookInput;
+    private Vector2 moveDireaction;
+
+
+
+    // 나중에 변경
+    private readonly float moveSpeed = 2f;
+
+    private void Awake()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animationController = GetComponentInChildren<PlayerAnimationController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        Look();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDireaction = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    private void Move()
+    {
+        rb.velocity = moveDireaction.normalized * moveSpeed;
+        if (moveDireaction != Vector2.zero)
+        {
+            animationController.MoveAnimation(true);
+        }
+        else
+        {
+            animationController.MoveAnimation(false);
+        }
+    }
+
+    private void Look()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(lookInput);
+        mousePos.z = 0;
+
+        if (mousePos.x < transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
